27a8ce64dab3fe314154000b8dc3b755130e4e1d
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-08-21 17:25:24 -0500

    Segunda prueba, corrigiendo el error de la lectura de las columnas.

--- leer_archivo.py
+++ leer_archivo.py
@@ -5,7 +5,11 @@ from io import BytesIO
 
 # Función para procesar el archivo Excel
 def procesar_excel(archivo):
-    df = pd.read_excel(archivo)
+    # Leer las columnas 'RECIBO' y 'VALOR' desde las celdas especificadas
+    df = pd.read_excel(archivo, header=1, usecols=["I", "K"])
+
+    # Renombrar las columnas para hacerlas más fáciles de manejar
+    df.columns = ['RECIBO', 'VALOR']
 
     # Crear un DataFrame vacío para almacenar los datos procesados
     df_nuevo = pd.DataFrame(columns=df.columns)
@@ -34,21 +38,22 @@ if archivo_subido is not None:
     # Procesar el archivo subido
     df_procesado = procesar_excel(archivo_subido)
 
-    # Mostrar la tabla procesada
-    st.write("Vista previa del archivo procesado:")
-    st.write(df_procesado)
+    if df_procesado is not None:
+        # Mostrar la tabla procesada
+        st.write("Vista previa del archivo procesado:")
+        st.write(df_procesado)
 
-    # Botón para descargar el archivo procesado
-    buffer = BytesIO()
-    with pd.ExcelWriter(buffer, engine='openpyxl') as writer:
-        df_procesado.to_excel(writer, index=False)
-        writer.save()
+        # Botón para descargar el archivo procesado
+        buffer = BytesIO()
+        with pd.ExcelWriter(buffer, engine='openpyxl') as writer:
+            df_procesado.to_excel(writer, index=False)
+            writer.save()
 
-    buffer.seek(0)
+        buffer.seek(0)
 
-    st.download_button(
-        label="Descargar archivo procesado",
-        data=buffer,
-        file_name='A' + archivo_subido.name,
-        mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    )
+        st.download_button(
+            label="Descargar archivo procesado",
+            data=buffer,
+            file_name='A' + archivo_subido.name,
+            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
