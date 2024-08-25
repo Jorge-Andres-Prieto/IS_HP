df8764175de5ecbe65fcc906f467f220ddffa19f
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-08-25 16:31:25 -0500

    16. este es el que funciona bien

--- leer_archivo.py
+++ leer_archivo.py
@@ -1,15 +1,8 @@
 import streamlit as st
 import pandas as pd
-import re
+import openpyxl
 
 def process_excel_file(uploaded_file):
-    """
-    Procesa un archivo Excel, separando los valores de RECIBO y VALOR, y creando nuevas filas.
-
-    Args:
-        uploaded_file: El archivo Excel subido por el usuario.
-    """
-
     # Leer el archivo de Excel
     df = pd.read_excel(uploaded_file, sheet_name='CORRIENTE')
 
@@ -19,20 +12,9 @@ def process_excel_file(uploaded_file):
     # Iterar sobre cada fila
     for index, row in df.iterrows():
         recibos = str(row['RECIBO']).split('-')
-        valor_str = str(row['VALOR'])
-
-        # Extraer los valores numéricos de la fórmula
-        match = re.search(r'=(\d+(\.\d+)?)([+-]?)(\d+(\.\d+)?)', valor_str)
-        if match:
-            valor1, op, valor2 = match.groups()
-            valores = [eval(valor1), eval(valor2)]
-        else:
-            valores = [valor_str]
-
-        for i in range(len(recibos)):
+        for recibo in recibos:
             new_row = row.copy()
-            new_row['RECIBO'] = recibos[i]
-            new_row['VALOR'] = valores[i]
+            new_row['RECIBO'] = recibo
             new_dfs.append(new_row)
 
     # Concatenar los nuevos dataframes en uno solo
