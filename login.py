910c080cc36887e8bf887604aba9a31d481d013a
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-08-26 04:14:57 -0500

    9

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-# Datos de usuarios y contraseñas (puedes cambiarlo a leer desde secrets)
+# Datos de usuarios y contraseñas
 USERS = {
     "usuario1": "password1",
     "usuario2": "password2",
@@ -10,9 +10,12 @@ def login():
     st.title("Login")
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
-    if st.button("Login"):
+    login_button = st.button("Login")
+
+    if login_button:
         if username in USERS and USERS[username] == password:
             st.session_state['logged_in'] = True
             st.session_state['username'] = username
+            st.experimental_rerun()  # Fuerza la recarga de la página para reflejar el cambio de estado
         else:
             st.error("Username or password is incorrect")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 from login import login
-from leer_archivo import main_program  # Asumiendo que el código principal está en una función llamada main_program
+from leer_archivo import main_program
 
 # Inicializar estado de sesión
 if 'logged_in' not in st.session_state:
@@ -9,24 +10,30 @@ if 'logged_in' not in st.session_state:
 def logout():
     st.session_state['logged_in'] = False
     st.session_state['username'] = None
+    st.experimental_rerun()  # Fuerza la recarga de la página para reflejar el cambio de estado
+
+def main_menu(user):
+    with st.sidebar:
+        selected = option_menu(
+            None,
+            ["Home", "Excel"],
+            icons=["house", "file-excel"],
+            menu_icon="list",
+            default_index=0
+        )
+        if st.button("Cerrar Sesión"):
+            logout()
+
+    if selected == "Home":
+        st.write("Bienvenido a HelPharma")
+    elif selected == "Excel":
+        main_program()
 
 def main():
     if not st.session_state['logged_in']:
         login()
     else:
-        with st.sidebar:
-            selected = st.selectbox(
-                "HelPharma",
-                ["Home", "Excel"],
-                index=0
-            )
-            if st.button("Cerrar Sesión"):
-                logout()
-
-        if selected == "Home":
-            st.write("Bienvenido a HelPharma")
-        elif selected == "Excel":
-            main_program()
+        main_menu(st.session_state['username'])
 
 if __name__ == "__main__":
     main()
