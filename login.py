05547d46300bd62f314540d821eb030ca69f77d1
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-08-26 04:44:13 -0500

    3

--- login.py
+++ login.py
@@ -7,14 +7,15 @@ USERS = {
 
 
 def login():
-    st.title("HelPharma")
-
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns([1, 2, 1])
 
     with col2:
+        st.title("HelPharma")
+        st.write("##")  # Añade un espacio vertical
+
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
-        login_button = st.button("Login")
+        login_button = st.button("Login", use_container_width=True)
 
         if login_button:
             if username in USERS and USERS[username] == password:
@@ -23,3 +24,6 @@ def login():
                 st.rerun()
             else:
                 st.error("Username or password is incorrect")
+
+    # Añadir un espacio vertical al final para empujar el contenido hacia arriba
+    st.write("##")
