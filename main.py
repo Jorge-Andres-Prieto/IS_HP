d999a7f7dec6904ac5cf13dedc86ac3280d20f25
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-08-26 03:45:13 -0500

    4

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ import streamlit as st
 from login import login
 from leer_archivo import main_program  # Importa tu programa principal
 
-# Inicializa el estado de sesión al iniciar
-if 'logged_in' not in st.session_state:
-    st.session_state['logged_in'] = False
 
 def main():
-    if not st.session_state['logged_in']:
-        login()
-    else:
-        with st.sidebar:
-            selected = st.selectbox(
-                "HelPharma",
-                ["Home", "Excel"],
-                index=0
-            )
-            if st.button("Cerrar Sesión"):
-                st.session_state['logged_in'] = False
+    # Inicializa el estado de sesión al iniciar
+    if 'logged_in' not in st.session_state:
+        st.session_state['logged_in'] = False
+        if not st.session_state['logged_in']:
+            login()
+        else:
+            with st.sidebar:
+                selected = st.selectbox(
+                    "HelPharma",
+                    ["Home", "Excel"],
+                    index=0
+                )
+                if st.button("Cerrar Sesión"):
+                    st.session_state['logged_in'] = False
 
-        if selected == "Home":
-            st.write("Bienvenido a HelPharma")
-        elif selected == "Excel":
-            main_program()
+            if selected == "Home":
+                st.write("Bienvenido a HelPharma")
+            elif selected == "Excel":
+                main_program()
 
 if __name__ == "__main__":
     main()
