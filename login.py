cd8a76cf3e047060ff99309ce0bb2d8c00d3319a
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-08-26 04:52:11 -0500

    5

--- login.py
+++ login.py
@@ -7,17 +7,35 @@ USERS = {
 
 
 def login():
+    st.set_page_config(layout="centered", page_title="HelPharma Login")
+
+    st.markdown("""
+    <style>
+    .big-font {
+        font-size:50px !important;
+        font-weight: bold;
+        color: #008fc4;
+        margin-bottom: 20px;
+    }
+    .stButton>button {
+        width: 100%;
+        background-color: #008fc4;
+        color: white;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
     col1, col2, col3 = st.columns([1, 2, 1])
 
     with col2:
-        st.title("HelPharma")
-        st.write("##")  # Añade un espacio vertical
+        st.markdown('<p class="big-font">HelPharma</p>', unsafe_allow_html=True)
 
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-        login_button = st.button("Login", use_container_width=True)
+        with st.form("login_form"):
+            username = st.text_input("Username")
+            password = st.text_input("Password", type="password")
+            submit_button = st.form_submit_button("Login")
 
-        if login_button:
+        if submit_button:
             if username in USERS and USERS[username] == password:
                 st.session_state['logged_in'] = True
                 st.session_state['username'] = username
@@ -25,5 +43,4 @@ def login():
             else:
                 st.error("Username or password is incorrect")
 
-    # Añadir un espacio vertical al final para empujar el contenido hacia arriba
-    st.write("##")
+    st.markdown('<div style="margin-top: 50px;"></div>', unsafe_allow_html=True)
